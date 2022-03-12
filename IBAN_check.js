9c4912f29bfec349265ad747bebdf6c4df44317d
Vytas Gadliauskas <vytasgadliauskas@gmail.com>
2022-03-12 06:35:12 +0200

    2

--- IBAN_check.js
+++ IBAN_check.js
@@ -32,120 +32,158 @@
         number = number.replace(/\s/g, '');
         ////////////// Nulinam reiksmes  
         ibanSallis = '';
-        numberlength = 0;
-        numberfirststrs = '';
-        numbersecondstrs = '';
+        ibanIlgis = 0;
+        ibanBBAN = '';
+        ibanFORM = '';
+        ibanOK = false;
         //
-        // numberlength = number.length;
-        numberfirststrs = number.slice(0,2);
-        numbersecondstrs = number.slice(2,4); 
+        console.log('------------------ debug-as --------------------');         
+         
         document.getElementById("testai").style.color = "black";
         document.getElementById("testai").textContent = '';
         document.getElementById("testai").textContent += '\n';
         document.getElementById("testai").textContent += '\n Tikrinamas IBAN : '+ number;
 
-        /////////////// Ilgis pagal sali, BBAN formatas
+        /////////////// 2022-03-12 dienos https://en.wikipedia.org/wiki/International_Bank_Account_Number  
         var ibanData = [
-            ["Andorra",        "AD", 28,"8n,16c"],
-            ["Albania",        "AL", 24,"8n,12c"],
-            ["Austria",        "AT", 20,"16n"],
-            ["Azerbaijan",     "AZ", 28,"4c,20n"],
-            ["",          "AT", "0  5n 0 ", "0  11n  0 "],
-            ["",          "AT", "0  5n 0 ", "0  11n  0 "],
-            ["",          "AT", "0  5n 0 ", "0  11n  0 "],
-            ["",          "AT", "0  5n 0 ", "0  11n  0 "],
-            ["",          "AT", "0  5n 0 ", "0  11n  0 "],
-            
- 
- 
-            ["Bosnia and Herzegovina", "BA", "0  3n 3n", "0   8n  2n"],
-            ["Belgium",        "BE", "0  3n 0 ", "0   7n  2n"],
-            ["Bulgaria",       "BG", "0  4a 4n", "2n  8   0 "],
-            ["Switzerland",    "CH", "0  5n 0 ", "0  12   0 "],
-            ["Cyprus",         "CY", "0  3n 5n", "0  16   0 "],
-            ["Czech Republic", "CZ", "0  4n 0 ", "0  16n  0 "],
-            ["Germany",        "DE", "0  8n 0 ", "0  10n  0 "],
-            ["Denmark",        "DK", "0  4n 0 ", "0   9n  1n"],
-            ["Estonia",        "EE", "0  2n 0 ", "2n 11n  1n"],
-            ["Spain",          "ES", "0  4n 4n", "2n 10n  0 "],
-            ["Finland",        "FI", "0  6n 0 ", "0   7n  1n"],
-            ["Faroe Islands",  "FO", "0  4n 0 ", "0   9n  1n"],
-            ["France",         "FR", "0  5n 5n", "0  11   2n"],
-            ["United Kingdom", "GB", "0  4a 6n", "0   8n  0 "],
-            ["Georgia",        "GE", "0  2a 0 ", "0  16n  0 "],
-            ["Gibraltar",      "GI", "0  4a 0 ", "0  15   0 "],
-            ["Greenland",      "GL", "0  4n 0 ", "0   9n  1n"],
-            ["Greece",         "GR", "0  3n 4n", "0  16   0 "],
-            ["Croatia",        "HR", "0  7n 0 ", "0  10n  0 "],
-            ["Hungary",        "HU", "0  3n 4n", "1n 15n  1n"],
-            ["Ireland",        "IE", "0  4a 6n", "0   8n  0 "],
-            ["Israel",         "IL", "0  3n 3n", "0  13n  0 "],
-            ["Iceland",        "IS", "0  4n 0 ", "2n 16n  0 "],
-            ["Italy",          "IT", "1a 5n 5n", "0  12   0 "],
-            ["Kuwait",         "KW", "0  4a 0 ", "0  22   0 "],
-            ["Kazakhstan",     "KZ", "0  3n 0 ", "0  13   0 "],
-            ["Lebanon",        "LB", "0  4n 0 ", "0  20   0 "],
-            ["Liechtenstein",  "LI", "0  5n 0 ", "0  12   0 "],
-            ["Lithuania",      "LT", "0  5n 0 ", "0  11n  0 "],
-            ["Luxembourg",     "LU", "0  3n 0 ", "0  13   0 "],
-            ["Latvia",         "LV", "0  4a 0 ", "0  13   0 "],
-            ["Monaco",         "MC", "0  5n 5n", "0  11   2n"],
-            ["Montenegro",     "ME", "0  3n 0 ", "0  13n  2n"],
-            ["Macedonia, Former Yugoslav Republic of", "MK", "0  3n 0 ", "0  10   2n"],
-            ["Mauritania",     "MR", "0  5n 5n", "0  11n  2n"],
-            ["Malta",          "MT", "0  4a 5n", "0  18   0 "],
-            ["Mauritius",      "MU", "0  4a 4n", "0  15n  3a"],
-            ["Netherlands",    "NL", "0  4a 0 ", "0  10n  0 "],
-            ["Norway",         "NO", "0  4n 0 ", "0   6n  1n"],
-            ["Poland",         "PL", "0  8n 0 ", "0  16n  0 "],
-            ["Portugal",       "PT", "0  4n 4n", "0  11n  2n"],
-            ["Romania",        "RO", "0  4a 0 ", "0  16   0 "],
-            ["Serbia",         "RS", "0  3n 0 ", "0  13n  2n"],
-            ["Saudi Arabia",   "SA", "0  2n 0 ", "0  18   0 "],
-            ["Sweden",         "SE", "0  3n 0 ", "0  16n  1n"],
-            ["Slovenia",       "SI", "0  5n 0 ", "0   8n  2n"],
-            ["Slovak Republic", "SK", "0  4n 0 ", "0  16n  0 "],
-            ["San Marino",     "SM", "1a 5n 5n", "0  12   0 "],
-            ["Tunisia",        "TN", "0  2n 3n", "0  13n  2n"],
-            ["Turkey",         "TR", "0  5n 0 ", "1  16   0 "]];
-                
-        //console.log(ibanData[0][1]);
-        //console.table(ibanData);
-        //console.log(numberfirststrs);
+            ["Albania",28,"8n,16c","AL kk bbb s sss x cccc cccc cccc cccc"],
+            ["Andorra",24,"8n,12c","AD kk bbbb ssss cccc cccc cccc"],
+            ["Austria",20,16n,"AT kk bbbb b ccc cccc cccc"],
+            ["Azerbaijan",28,"4c,20n","AZ kk bbbb cccc cccc cccc cccc cccc"],
+            ["Bahrain",22,"4a,14c","BH kk bbbb cccc cccc cccc cc"],
+            ["Belarus",28,"4c, 4n, 16c","BY kk bbbb aaaa cccc cccc cccc cccc"],
+            ["Belgium",16,12n,"BE kk bbb c cccc cc xx"],
+            ["Bosnia and Herzegovina",20,16n,"BA kk bbb s ss cc cccc cc xx"],
+            ["Brazil",29,"23n,1a,1c","BR kk bbbb bbbb ssss s ccc cccc ccc t n"],
+            ["Bulgaria",22,"4a,6n,8c","BG kk bbbb ssss tt cc cccc cc"],
+            ["Costa Rica",22,18n,"CR kk 0 bbb cccc cccc cccc cc"],
+            ["Croatia",21,17n,"HR kk bbbb bbb c cccc cccc c"],
+            ["Cyprus",28,"8n,16c","CY kk bbb s ssss cccc cccc cccc cccc"],
+            ["Czech Republic",24,20n,"CZ kk bbbb ssss ss cc cccc cccc"],
+            ["Denmark",18,14n,"DK kk bbbb cccc cccc c x"],
+            ["Dominican Republic",28,"4a,20n","DO kk bbbb cccc cccc cccc cccc cccc"],
+            ["East Timor",23,19n,"TL kk bbb c cccc cccc cccc c xx"],
+            ["Egypt",29,25n,"EG kk bbbb ssss cccc cccc cccc cccc c"],
+            ["El Salvador",28,"4a, 20n","SV kk bbbb cccc cccc cccc cccc cccc"],
+            ["Estonia",20,16n,"EE kk bb ss cccc cccc ccc x"],
+            ["Faroe Islands",18,14n,"FO kk bbbb cccc cccc c x"],
+            ["Finland",18,14n,"FI kk bbbb bb cc cccc c x"],
+            ["France",27,"10n,11c,2n","FR kk bbbb b sss ss cc cccc cccc c xx"],
+            ["Georgia",22,"2c,16n","GE kk bb cc cccc cccc cccc cc"],
+            ["Germany",22,18n,"DE kk bbbb bbbb cccc cccc cc"],
+            ["Gibraltar",23,"4a,15c","GI kk bbbb cccc cccc cccc ccc"],
+            ["Greece",27,"7n,16c","GR kk bbb s sss c cccc cccc cccc ccc"],
+            ["Greenland",18,14n,"GL kk bbbb cccc cccc c x"],
+            ["Guatemala",28,"4c,20c","GT kk bbbb mm tt cccc cccc cccc cccc"],
+            ["Hungary",28,24n,"HU kk bbb s sss x cccc cccc cccc ccc x"],
+            ["Iceland",26,22n,"IS kk bb ss tt cc cccc iiii iiii ii"],
+            ["Iraq",23,"4a,15n","IQ kk bbbb sss c cccc cccc ccc"],
+            ["Ireland",22,"4c,14n","IE kk aaaa bbbb bb cc cccc cc"],
+            ["Israel",23,19n,"IL kk bbb s ss cc cccc cccc ccc"],
+            ["Italy",27,"1a,10n,12c","IT kk x bbb bb ss sss c cccc cccc ccc"],
+            ["Jordan",30,"4a,22n","JO kk bbbb ssss cccc cccc cccc cccc cc"],
+            ["Kazakhstan",20,"3n,13c","KZ kk bbb c cccc cccc cccc"],
+            ["Kosovo",20,"4n,10n,2n","XK kk bbbb cccc cccc cccc"],
+            ["Kuwait",30,"4a,22c","KW kk bbbb cccc cccc cccc cccc cccc cc"],
+            ["Latvia",21,"4a,13c","LV kk bbbb cccc cccc cccc c"],
+            ["Lebanon",28,"4n,20c","LB kk bbbb cccc cccc cccc cccc cccc"],
+            ["Libya",25,21n,"LY kk bbb s ss cc cccc cccc cccc c"],
+            ["Liechtenstein",21,"5n,12c","LI kk bbbb b ccc cccc cccc c"],
+            ["Lithuania",20,16n,"LT kk bbbb b ccc cccc cccc"],
+            ["Luxembourg",20,"3n,13c","LU kk bbb c cccc cccc cccc"],
+            ["North Macedonia",19,"3n,10c,2n","MK kk bbb c cccc cccc c xx"],
+            ["Malta",31,"4a,5n,18c","MT kk bbbb ssss s ccc cccc cccc cccc ccc"],
+            ["Mauritania",27,23n,"MR kk bbbb b sss ss cc cccc cccc c xx"],
+            ["Mauritius",30,"4a,19n,3a","MU kk bbbb bb ss cccc cccc cccc 000m mm"],
+            ["Monaco",27,"10n,11c,2n","MC kk bbbb b sss ss cc cccc cccc c xx"],
+            ["Moldova",24,"2c,18c","MD kk bb cc cccc cccc cccc cccc"],
+            ["Montenegro",22,18n,"ME kk bbb c cccc cccc cccc xx"],
+            ["Netherlands",18,"4a,10n","NL kk bbbb cccc cccc cc"],
+            ["Norway",15,11n,"NO kk bbbb cccc cc x"],
+            ["Pakistan",24,"4c,16n","PK kk bbbb cccc cccc cccc cccc"],
+            ["Palestinian territories",29,"4c,21n","PS kk bbbb cccc cccc cccc cccc cccc c"],
+            ["Poland",28,24n,"PL kk bbb s sss x cccc cccc cccc cccc"],
+            ["Portugal",25,21n,"PT kk bbbb ssss cccc cccc ccc x x"],
+            ["Qatar",29,"4a,21c","QA kk bbbb cccc cccc cccc cccc cccc c"],
+            ["Romania",24,"4a,16c","RO kk bbbb cccc cccc cccc cccc"],
+            ["Saint Lucia",32,"4a,24c","LC kk bbbb cccc cccc cccc cccc cccc cccc"],
+            ["San Marino",27,"1a,10n,12c","SM kk x bbb bb ss sss c cccc cccc ccc"],
+            ["São Tomé and Príncipe",25,21n,"ST kk bbbb ssss cccc cccc cccc c"],
+            ["Saudi Arabia",24,"2n,18c","SA kk bb cc cccc cccc cccc cccc"],
+            ["Serbia",22,18n,"RS kk bbb c cccc cccc cccc xx"],
+            ["Seychelles",31,"4a,20n,3a","SC kk bbbb bb ss nnnn nnnn nnnn nnnn mmm"],
+            ["Slovakia",24,20n,"SK kk bbbb ssss ss cc cccc cccc"],
+            ["Slovenia",19,15n,"SI kk bb ss s ccc cccc c xx"],
+            ["Spain",24,20n,"ES kk bbbb ssss xx cc cccc cccc"],
+            ["Sudan",18,14n,"SD kk bb cc cccc cccc cc"],
+            ["Sweden",24,20n,"SE kk bbb c cccc cccc cccc cccc"],
+            ["Switzerland",21,"5n,12c","CH kk bbbb b ccc cccc cccc c"],
+            ["Tunisia",24,20n,"TN kk bb ss s ccc cccc cccc cc xx"],
+            ["Turkey",26,"5n,17c","TR kk bbbb b 0 cc cccc cccc cccc cc"],
+            ["Ukraine",29,"6n, 19c","UA kk bbbb bb cc cccc cccc cccc cccc c"],
+            ["United Arab Emirates",23,"3n,16n","AE kk bbb c cccc cccc cccc ccc"],
+            ["United Kingdom",22,"4a,14n","GB kk bbbb ssss ss cc cccc cc"],
+            ["Vatican City",22,"3n,15n","VA kk bbb c cccc cccc cccc cc"],
+            ["Virgin Islands, British",24,"4c,16n","VG kk bbbb cccc cccc cccc cccc"]
+            ];
+              
 
         for (let i = 0; i < ibanData.length; i++) {
-            if (numberfirststrs == ibanData[i][1]) {
-                ibanSallis = ibanData[i][0];
-                
+            if (number.slice(0,2) == ibanData[i][3].slice(0,2)) {
+                ibanSalis = ibanData[i][0];
+                ibanIlgis = ibanData[i][1];
+                ibanBBAN = ibanData[i][2];
+                ibanFORM = ibanData[i][3];
+
+
+                console.log('ibanData : ', ibanData[i]);
+                console.log('number1 : ', number.slice(0,2));
+                console.log('number2 : ', number.slice(2,4));
+                console.log('number-length : ', number.length);
+                console.log('ibanSalis : ', ibanSalis);  
+                console.log('ibanIlgis : ', ibanIlgis);
+                console.log('ibanBBAN : ', ibanBBAN);
+                console.log('ibanFORM : ', ibanFORM);
             }
         } 
 
+        ///////////////////////////////////////////////////////// TESTAI 
+        /////////////////  Pagal pirmus simbolius   
+        if (allLetter(number.slice(0,2))) {
+            ibanOK = true;
+                /////////////////////////  Pagal salies kodo ilgi
+                if  (number.length == ibanIlgis) {
+                    /////////////////////////  Pagal antrinius simbolius
+                    if (isNum(number.slice(2,4))) {
+                        ////////////////////////   mod-97
 
-        document.getElementById("testai").textContent += '\n IBAN salis : '+ ibanSallis;
+                    } else {
+                        ibanOK = false;    
+                    }
 
-        document.getElementById("testai").textContent += '\n IBAN simboliu skaicius : '+ number.length;
-
-        /////////////////  Pagal pirmus simbolius RAIDES   
-        if (allLetter(numberfirststrs)) {
-            document.getElementById("testai").textContent += '\n IBAN salis : '+ numberfirststrs;    
+                } else {
+                    ibanOK = false;    
+                }
         } else {
-            document.getElementById("testai").textContent += '\n IBAN kodo KLAIDA (KODAS NETEISINGAS !!!)';
-            document.getElementById("testai").style.color = "red";
+            ibanOK = false;    
         }
-        //////////////////  Pagal antrus simbolius SKAICIAI
-        if (isNum(numbersecondstrs)) {
-            document.getElementById("testai").textContent += '\n IBAN testas : CHECK DIGIT'    
+
+        ////////////////  Output'as
+        console.log('ibanOK : ', ibanOK);
+        if (ibanOK) {
+            document.getElementById("testai").textContent += '\n IBAN testas : CHECK IBAN FORMAT OK'
+            document.getElementById("testai").textContent += '\n IBAN testas : CHECK COUNTRY DIGIT OK'
+            document.getElementById("testai").textContent += '\n IBAN testas : CHECK COUNTRY FORMAT - not implemented'
+            document.getElementById("testai").textContent += '\n IBAN testas : CHECK MOD-97 DIGIT OK'
+            document.getElementById("testai").textContent += '\n IBAN salis : '+ ibanSalis; 
+            
+            document.getElementById("testai").textContent += '\n IBAN OK'
+            document.getElementById("testai").style.color = "green";    
         } else {
             document.getElementById("testai").textContent += '\n IBAN kodo KLAIDA (KODAS NETEISINGAS -!!!)';
             document.getElementById("testai").style.color = "red";
         }
-        
-        
-        
-        //////////////// mod-97
-          
-        
-        document.getElementById("testai").textContent += '\n IBAN mod-97 : ';
+        ////////////////   HAPPY END I AM PROUD OF MYSELF :)   mail.: vytasgadliauskas@gmail.com
     }
 
 
